f691b3acbbac22030a88a2235afb34ea09590a9f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-03-31 18:40:32 +0200

    Objetos e interfaces

---
+++ 01-TypeScript-intro/src/bases/02-objects.ts
@@ -0,0 +1,32 @@
+export const pokemonIds = [1, 20, 30, 34, 66];
+
+// Conversión rápida de string a number
+// Otra forma sería usando Number('1')
+pokemonIds.push(+'1');
+
+console.log(pokemonIds);
+
+// Creando interfaces
+interface Pokemon {
+  id: number;
+  name: string;
+  age?: number; // opcional con ?
+}
+
+// Objetos literales
+export const bulbasaur: Pokemon = {
+  id: 1,
+  name: 'Bulbasaur',
+  age: 2,
+};
+
+console.log(bulbasaur);
+
+// Las interfaces no se pueden instanciar
+//const charmander = new Pokemon();
+
+export const charmander: Pokemon = {
+  id: 4,
+  name: 'Charmander',
+  age: 1,
+};
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,26 +1,13 @@
 import { name, age } from './bases/01-types';
+import { pokemonIds, bulbasaur } from './bases/02-objects';
 import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Hello ${name}, ${age}</h1>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>01-types</h1>
+    <h2>Hello ${name}, ${age}!</h2>
+    <h1>02-objects</h1>
+    <h2>Hello ${pokemonIds.join(', ')}</h2>
+    <h2>${bulbasaur.name}</h2>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
