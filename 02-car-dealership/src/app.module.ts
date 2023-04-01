4ef69943e06b68b4a58e7f89c208c6b99895fa68
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-01 15:13:39 +0200

    Creado modulo cars

--- 02-car-dealership/src/app.module.ts
+++ 02-car-dealership/src/app.module.ts
@@ -1,9 +1,10 @@
 // Este es el módulo principal.
 // Va a tener las referencias a todos los otros módulos, servicios, etc. que conforman mi aplicación.
 import { Module } from '@nestjs/common';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [CarsModule],
   controllers: [],
   providers: [],
   exports: [],
