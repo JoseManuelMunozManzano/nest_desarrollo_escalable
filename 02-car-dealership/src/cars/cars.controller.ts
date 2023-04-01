0e9406e4f6d457f08f886eaa203b78ace5c1f9ca
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-01 15:55:53 +0200

    2-Crea servicio e inyección de dependencias

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 // El controlador escucha las solicitudes de los clientes y emite una respuesta
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  // Inyección de dependencia.
+  // Para que Nest cree la instancia de CarsController tiene la dependencia de CarsService.
+  // Nest creará (si no existe ya porque en tal caso la reutiliza por motivos de eficiencia) la
+  // instancia de CarsService.
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
-    return { car: this.cars[id] };
+    return this.carsService.findOneById(+id);
   }
 }
