90437bb55d75e120f246836a066b27c1512a097f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-02 05:59:33 +0200

    2-Pipes-transformacion de la data

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 // El controlador escucha las solicitudes de los clientes y emite una respuesta
@@ -15,8 +15,17 @@ export class CarsController {
     return this.carsService.findAll();
   }
 
+  // Tenemos que validar que el id sea un número, y de hecho queremos que sea un número de entrada,
+  // no el string que entra por defecto, y no tener que transformarlo nosotros.
+  // Habría que realizar un manejo de excepciones, pero Nest ya nos puede ayudar con ello (Pipes).
+  // Los pipes transforman la data recibida en los requests.
+  // Con @UsePipes indicamos que usamos pipes. Se usa ValidationPipe para validar la información del request.
+  // Se pueden informar varios pipes en cadena.
+  // Si ahora se mandara un texto, por ejemplo: http://localhost:3000/cars/1
+  // daría un status 400 (antes de usar pipes era 200) con un mensaje indicando que se espera un
+  // número
   @Get(':id')
-  getCarById(@Param('id') id: string) {
-    return this.carsService.findOneById(+id);
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
   }
 }
