a55fff772857ebe33fa95a0b2f751a67056881fe
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-02 06:11:57 +0200

    2-Exception Filters si no existe el id

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -5,7 +5,7 @@
 //
 // Los providers son clases que se pueden inyectar. Pero no todos los providers van a tener
 // lógica de negocio (no todos son servicios)
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -32,8 +32,20 @@ export class CarsService {
     return this.cars;
   }
 
+  // Sigue habiendo otro error.
+  // Si se manda como id un número que no existe devuelve un status 200 en vez de un 404.
+  // Para corregir esto, Nest dispone de varios Exception Filters.
+  // Como nos encontramos dentro de la exception zone definida por Nest, Nest se encarga de manejar
+  // las excepciones que lancemos (throw).
+  // La Exception zone incluye:
+  //   Guards -> Before Interceptor -> Pipes -> Controllers -> Decorators -> After Interceptor
   findOneById(id: number) {
     const car = this.cars.find((car) => car.id === id);
+
+    // Para probar en Postman ir a la ruta: localhost:3000/cars/4
+    if (!car)
+      http: throw new NotFoundException(`Car with id '${id}' not found`);
+
     return car;
   }
 }
