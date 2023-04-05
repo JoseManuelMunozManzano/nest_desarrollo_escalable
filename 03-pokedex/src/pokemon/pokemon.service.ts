c3772e343fd337b48481ef9a74cc3d742f455c62
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-05 16:17:47 +0200

    3-@InjectModel y crear Pokemon en BD

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  // Inyección de dependencias del modelo, haciendo referencia a nuestra entity Pokemon.
+  // El modelo por si solo no es inyectable porque no es un Provider, de ahí que se use el decorador
+  // @InjectModel con el nombre de ese modelo.
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  // Las inserciones en la BD siempre son asíncronas.
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
 
-    return createPokemonDto;
+    // Falta agregar validaciones.
+    // PROBLEMA: Si se intenta grabar dos veces el mismo no o el mismo name da error 500 sin indicar una
+    // información de que salió mal.
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+    return pokemon;
   }
 
   findAll() {
