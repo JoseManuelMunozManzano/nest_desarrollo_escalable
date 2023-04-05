3a327cdc8d949c2be9706de42f876242b4a7fb2e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-05 16:03:35 +0200

    3-dto, validaciones POST y recibir data

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // En vez de indicar la url /api/v2 en pokemon.controller.ts lo hacemos aquí, a nivel de aplicación.
   app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
--- 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePokemonDto {}
+// La data que vamos a recibir en un POST
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+// La tenemos que validar, por lo que tenemos que instalar: yarn add class-validator class-transformer
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
