1049beaac823b3123da78b7c95b524c2227e981a
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-08 17:25:31 +0200

    3-Paginación. Uso de query parameters

--- 03-pokedex/src/pokemon/pokemon.controller.ts
+++ 03-pokedex/src/pokemon/pokemon.controller.ts
@@ -8,11 +8,13 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  Query,
 } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 // La url debe contener /api/v2
 // No es bueno indicar en el @Controler /api/v2/pokemon, porque si tuviéramos unos 10 endpoints tendríamos que hacer
@@ -30,8 +32,25 @@ export class PokemonController {
     return this.pokemonService.create(createPokemonDto);
   }
 
+  // Para poder paginar hay que jugar con los métodos de mongo .limit(x) y .skip(y)
+  // limit(5) devuelve los siguientes 5 documentos.
+  // skip(3) significa que empezaremos a devolver a partir del 4 documento.
+  //
+  // Necesitaremos pasar estos valores como query parameters.
+  //
+  // Para obtener todos los query parameters usaremos el decorador @Query()
+  // Siempre vendrán como string.
+  // El problema de estos query parameters es que son opcionales y que debemos validarlos.
+  // Usaremos un nuevo dto para ello y como va a ser muy genérico lo crearemos en el módulo common.
+  //
+  // PROBLEMA: En Postman, esta url: http://localhost:3000/api/v2/pokemon?limit=10
+  // provoca los siguientes errores:
+  //          "limit must not be less than 1",
+  //          "limit must be a positive number";
+  // porque los query parameters llegan como string y no hay ninguna conversión a número.
   @Get()
-  findAll() {
+  findAll(@Query() paginationDto: PaginationDto) {
+    console.log({ paginationDto });
     return this.pokemonService.findAll();
   }
 
