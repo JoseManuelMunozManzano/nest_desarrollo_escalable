fedd004668c5647cc8ca1657707387534a5bdfcd
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-08 08:13:46 +0200

    3-Realizar peticiones http desde Nest con axios

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,8 +1,29 @@
+// Se instala axios
+//  yarn add axios
+// para recuperar la data (también se puede usar fetch directamente desde la versión 18 de Node)
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  // Luego se implementará usando el patrón adaptador y un custom provider para poder sustituir axios por request o
+  // fetch API o cualquier paquete que nos sirva para generar peticiones http.
+  // Así no tendremos aquí esta dependencia.
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
 }
