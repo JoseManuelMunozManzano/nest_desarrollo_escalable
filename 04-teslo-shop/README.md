8897fb565ab27b88399aff23c3c00fd2d8a6aae9
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-10 18:20:46 +0200

    4-Conectar Postgres con Nest

--- 04-teslo-shop/README.md
+++ 04-teslo-shop/README.md
@@ -10,10 +10,25 @@ Proyecto creado con:
 nest new 04-teslo-shop
 ```
 
-1. Levantar la base de datos
+1. Clonar proyecto
+2. Instalar dependencias
+
+```
+  yarn install
+```
+
+3. Clonar el archivo `.env.template` y renombrarlo a `.env`
+4. Cambiar las variables de entorno
+5. Levantar la base de datos
 
 ```
 docker-compose up -d
+```
+
+6. Levantar el modo desarrollo
+
+```
+  yarn start:dev
 ```
 
 Se ha visto:
--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,6 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Para conectar Nest a la BD de tipo relacional ver la siguiente página:
+// https://docs.nestjs.com/techniques/database
+// Hay que instalar:
+// yarn add @nestjs/typeorm typeorm pg
 
 @Module({
-  imports: [],
+  imports: [
+    // Para poder usar variables de entorno (.env) en Nest
+    ConfigModule.forRoot(),
+    // Configuración del TypeOrm usando Postgres
+    // NOTA: Se usa forFeature() cuando queremos expandir funcionalidades.
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      // Para que cargue automáticamente las entidades que vamos definiendo.
+      autoLoadEntities: true,
+      // Esta propiedad a true solo para desarrollo. No se suele usar en Producción.
+      // Cuando se borra una columna automáticamente la sincroniza.
+      // Se puede usar una variable de entorno para gestionar el true o false.
+      // En Producción se suelen hacer procesos de migraciones cuando cambia un archivo.
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
