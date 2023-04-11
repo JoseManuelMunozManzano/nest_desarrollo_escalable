c4e588a8609cda6083c963a11310de09f70dcc09
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-11 19:36:24 +0200

    4-manejo de errores global

---
+++ 04-teslo-shop/src/common/services/error-handle.service.ts
@@ -0,0 +1,29 @@
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+
+@Injectable()
+export class ErrorHandleService {
+  private readonly logger: Logger;
+
+  constructor(@Inject() private serviceName: string) {
+    this.logger = new Logger(serviceName);
+  }
+
+  public errorHandle(error: any) {
+    if (error.code === '23505') {
+      this.logger.error(`${error} - ${error.detail}`);
+      throw new BadRequestException(error.detail);
+    }
+
+    // Usando el logger. Se ve en consola de Nest
+    this.logger.error(`${error} - ${error.detail}`);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
+  }
+}
--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,20 +1,17 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-  Logger,
-} from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
-import e from 'express';
+import { ErrorHandleService } from 'src/common/services/error-handle.service';
 
 @Injectable()
 export class ProductsService {
-  // Para ver mejor los errores usaremos el logger de Nest
-  private readonly logger = new Logger('ProductsService');
+  // Usamos el servicio de manejo de errores
+  private readonly errorHandler: ErrorHandleService = new ErrorHandleService(
+    'ProductsService',
+  );
 
   // El patrón repositorio ya viene definido por defecto en Nest. Antes había que hacer una clase independiente.
   // Para inyectar un repositorio se usa el decorador @InjectRepository e inyectaremos aquí nuestra entidad entre
@@ -46,8 +43,8 @@ export class ProductsService {
       // Regresamos el producto.
       return product;
     } catch (error) {
-      // Tratando los errores de una mejor forma. Con logs y método privado dedicado.
-      this.handleDBExceptions(error);
+      // Tratando los errores de una mejor forma. Con logs centralizados en un servicio
+      this.errorHandler.errorHandle(error);
     }
   }
 
@@ -65,20 +62,5 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
-  }
-
-  // error de tipo any porque quiero manejar cualquier tipo de error que venga.
-  private handleDBExceptions(error: any) {
-    //console.log(error);
-    if (error.code === '23505') {
-      this.logger.error(error);
-      throw new BadRequestException(error.detail);
-    }
-
-    // Usando el logger. Se ve en consola de Nest
-    this.logger.error(error);
-    throw new InternalServerErrorException(
-      'Unexpected error, check server logs',
-    );
   }
 }
