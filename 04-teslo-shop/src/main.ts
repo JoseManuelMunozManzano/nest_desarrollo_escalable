7019cbd83ebe20d5df502e9e6e3cfeaf0eb944ec
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-11 06:55:20 +0200

    4-Crear el DTO create-product.dto.ts

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Para que las urls tengan /api como prefijo
+  app.setGlobalPrefix('api');
+
+  // GlobalPipe para poder usar las validaciones/transformaciones de DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT, () => {
     console.log(`App running on port ${process.env.PORT}`);
   });
--- 04-teslo-shop/src/products/dto/create-product.dto.ts
+++ 04-teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,1 +1,47 @@
-export class CreateProductDto {}
+// La información que estamos esperando en el request para hacer el create del producto.
+// Recordar que para poder usar los decoradores de validaciones hay que instalar 2 paquetes:
+// yarn add class-validator class-transformer
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  // Hay que indicar @IsOptional() porque la interrogación indica que es opcional SOLO PARA TYPESCRIPT!
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  // Cada uno de los elementos del array tiene que ser un string, de ahí el each a true
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  // Se esperan estos valores
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
