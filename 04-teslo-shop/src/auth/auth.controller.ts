dec755a1f927929ba6205244e877ac98452e46af
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-05-08 06:50:01 +0200

    4-Autorización-Private Route - General

--- 04-teslo-shop/src/auth/auth.controller.ts
+++ 04-teslo-shop/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 // Recordar que los controladores son los que escuchan los requests y emiten una respuesta.
-import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
+import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 
 @Controller('auth')
@@ -16,5 +17,25 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  // Creamos la primera ruta privada.
+  // Su misión es asegurarse de que el JSON Web Token esté presente en el header como un bearer token,
+  // Que el usuario exista, esté activo y el token no haya expirado.
+  // Primero la vamos a poner aquí en el controller, pero luego la llevaremos a su sitio definitivo.
+  //
+  // Para hacer una ruta privada tenemos que implementar Guards, que se usan para permitir o prevenir acceso
+  // a una ruta (aquí es donde se debe autorizar una solicitud)
+  // Primero se hace aquí con AuthGuard(), que ya lo hace todo automático, y luego se harán Guards personalizados.
+  // Una vez hecho esto, en Postman con este Get: http://localhost:3001/api/auth/private
+  // el resultado será 401, Unauthorized SI NO TENEMOS TOKEN O NO TENEMOS IMPLEMENTADO AUTHORIZATION
+  // CON BEARER TOKEN O EL USUARIO ESTA INACTIVO.
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute() {
+    return {
+      ok: true,
+      mesage: 'Hola Mundo Private',
+    };
   }
 }
